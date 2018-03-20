3a18d0138a5ba551a5503ca75e0f4ae218a4ad9c
Rachel Miller <raeanne1991@gmail.com>
2018-03-20 23:53:08 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,7 +20,7 @@ class SongsController < ApplicationController
 
   def update
     @song = Song.find(params[:id])
-    @song.update(params.require(:song).permit(:artist_id, :genre_id))
+    @song.update(params.require(:song).permit(:name, :artist_id, :genre_id))
     redirect_to song_path(@song)
   end
 
